78e7ee7584b24793a73c5fb4156841d1f2735f6e
JuanLamas <juanedulam2@gmail.com>
2025-05-11 22:53:51 -0300

    Modificando MostrarProducto - Lamas juan

--- src/assets/components/MostrarProducto.jsx
+++ src/assets/components/MostrarProducto.jsx
@@ -5,9 +5,14 @@ function MostrarProducto (props) {
     const [producto,setProducto] = useState(props.producto);
 
     return(
-        <div>
-            <p>ID: {producto.id} - {producto.descripcion} - ${producto.precioUnitario} - Stock {producto.stock}</p>
-        </div>
+            <tr key={producto.id}>
+            <td>{producto.id}</td>
+            <td>{producto.descripcion}</td>
+            <td>${producto.precioUnitario}</td>
+            <td>{producto.descuento}%</td>
+            <td>${producto.precioConDescuento.toFixed(2)}</td>
+            <td>{producto.stock}</td>
+            </tr>
     )
 } 
 
